56597632fc2652a9dfe2efc245a9b9a86fe130a8
nazgull08 <nazgull08@protonmail.com>
2023-03-26 03:11:39 +0300

    added some types and balances list with tx height, this solution is draft

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ pub extern crate elements_miniscript as miniscript;
 pub extern crate serde;
 pub extern crate log;
 
+use bdk::TransactionDetails;
 use miniscript::bitcoin::network::constants::Network;
 use miniscript::bitcoin::util::bip32::{ExtendedPrivKey, ExtendedPubKey};
 use miniscript::elements::confidential::{
@@ -44,6 +45,18 @@ pub enum ScriptType {
 pub struct DownloadTxResult {
     pub txs: Vec<(elements::Txid, elements::Transaction)>,
     pub unblinds: Vec<(elements::OutPoint, elements::TxOutSecrets)>,
+}
+
+#[derive(Default,Debug,Clone)]
+pub struct EDKTransactionDetails {
+    pub txs: Vec<(elements::Txid, elements::Transaction, i32)>,
+    pub unblinds: Vec<(elements::OutPoint, elements::TxOutSecrets)>,
+}
+
+#[derive(Debug,Clone)]
+pub struct TxWithHeight{
+    tx : elements::Transaction,
+    tx_height : i32
 }
 
 pub struct Wallet<D> {
@@ -130,6 +143,12 @@ where
         Ok(self.get_previous_addresses()?.contains(addr))
     }
 
+    pub fn list_transactions(&self) -> Result<EDKTransactionDetails,bdk::Error> {
+        let addrs: Vec<Address> = self.get_previous_addresses()?;
+        Ok(self.balance_addresses_with_height(addrs)?)
+        
+    }
+
     pub fn balance(&self) -> Result<HashMap<String, u64>, bdk::Error> {
         let addrs: Vec<Address> = self.get_previous_addresses()?;
         let mut balances = HashMap::new();
@@ -143,6 +162,90 @@ where
         }
         info!("======Checking for ========");
         Ok(balances)
+    }
+
+    pub fn balance_addresses_with_height(&self, addrs: Vec<Address>) -> Result<EDKTransactionDetails, bdk::Error> {
+        //let client = Client::new("ssl://blockstream.info:995").unwrap();
+
+        let mut history_txs_id = HashSet::<elements::Txid>::new();
+
+        let scripts: Vec<elements::Script> = addrs
+            .iter()
+            .map(|x| x.script_pubkey().into_bytes())
+            .map(|x| elements::Script::from(x))
+            .collect();
+        let b_scripts: Vec<bitcoin::Script> = addrs
+            .iter()
+            .map(|x| x.script_pubkey().into_bytes())
+            .map(|x| bitcoin::Script::from(x))
+            .collect();
+        let result: Vec<Vec<GetHistoryRes>> = self
+            .client
+            .batch_script_get_history(b_scripts.iter())
+            .unwrap();
+        let flattened: Vec<GetHistoryRes> = result.into_iter().flatten().collect();
+        Ok(self.download_txs_with_height(flattened, &scripts)?)
+    }
+
+
+
+    fn download_txs_with_height(
+        &self,
+        history_txs_id: Vec<GetHistoryRes>,
+        scripts: &Vec<elements::Script>,
+    ) -> Result<EDKTransactionDetails, bdk::Error> {
+        let mut txs = vec![];
+        let mut unblinds = vec![];
+        // BETxid has to be converted into bitcoin::Txid for rust-electrum-client
+        let txs_to_download: Vec<bitcoin::Txid> = history_txs_id
+            .iter()
+            .map(|x| x.tx_hash)
+            .collect();
+        let txs_height: Vec<i32> = history_txs_id
+            .iter()
+            .map(|x| x.height)
+            .collect();
+        if txs_to_download.is_empty() {
+            Ok(EDKTransactionDetails::default())
+        } else {
+            let txs_bytes_downloaded = self
+                .client
+                .batch_transaction_get_raw(txs_to_download.iter())
+                .unwrap();
+            let mut txs_downloaded: Vec<elements::Transaction> = vec![];
+
+            for vec in txs_bytes_downloaded {
+                let tx: elements::Transaction = elm_des(&vec).unwrap();
+                txs_downloaded.push(tx);
+            }
+            println!("txs_downloaded {}", txs_downloaded.len());
+            let txs_downloaded_with_height:Vec<TxWithHeight> = txs_downloaded.iter()
+                                                            .zip(txs_height.iter())
+                                                            .map(|(t,h)| TxWithHeight{tx: t.to_owned(), tx_height:h.to_owned()})
+                                                            .collect();
+            //let mut previous_txs_to_download = HashSet::new();
+            for tx in txs_downloaded_with_height.into_iter() {
+                let txid = tx.tx.txid();
+                println!("compute OutPoint Unblinded");
+                for (i, output) in tx.tx.output.iter().enumerate() {
+                    let script = output.script_pubkey.clone();
+                    if scripts.contains(&script) {
+                        let vout = i as u32;
+                        let outpoint = elements::OutPoint {
+                            txid: tx.tx.txid(),
+                            vout,
+                        };
+                        match self.try_unblind(outpoint, output.clone()) {
+                            Ok(unblinded) => unblinds.push((outpoint, unblinded)),
+                            Err(_) => println!("{} cannot unblind, ignoring (could be sender messed up with the blinding process)", outpoint),
+                        }
+                    }
+                }
+                txs.push((txid, tx.tx, tx.tx_height));
+            }
+
+            Ok(EDKTransactionDetails { txs, unblinds })
+        }
     }
 
     pub fn balance_addresses(&self, addrs: Vec<Address>) -> Result<DownloadTxResult, bdk::Error> {
